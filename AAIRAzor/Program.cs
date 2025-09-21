9fe98ba8486875cce3a7f09e19dc9a8074f75fef
Richard Chalk <barmybrit@gmail.com>
2025-09-21 21:08:07 +0200

    Added comment about Threads

--- AAIRAzor/Program.cs
+++ AAIRAzor/Program.cs
@@ -12,7 +12,18 @@ namespace AAIRAzor
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
             // Lägg till DI för min Azure OpenAI service            
+
+            // 'Singleton' är rätt här:
+            // AzureOpenAIClient är 'trådsäker*' och gjorda för att återanvändas,
+            // vilket sparar resurser och återanvänder HTTP-anslutningar
+            // istället för att skapa nya hela tiden.
+
+            // *Trådsäker betyder att flera trådar kan använda samma instans samtidigt utan problem.
+            // tex: Om två användare samtidigt skickar förfrågningar till din
+            // webapp och båda använder samma AzureOpenAIClient,
+            // så kommer den att hantera båda parallellt utan problem.
             builder.Services.AddSingleton(sp =>
                 new AzureOpenAIClient(
                     new Uri("https://systementoropenaiinstance.openai.azure.com/"),
